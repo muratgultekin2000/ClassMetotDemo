05479b4bfdf6afeee8a210e8684a761f896b10ff
murat gültekin <muratgultekin2000@hotmail.com>
2021-01-20 20:23:07 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Müşterini Kaydı Eklenmiştir.");
+        }
+        public void MusteriSil(Musteri musteri) 
+        {
+            Console.WriteLine(musteri.Ad + " " +musteri.Soyad + " Müşterini Kaydı Silinmiştir.");
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "ali";
+            musteri1.Soyad = "kemal";
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "veli";
+            musteri2.Soyad = "can";
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "şaban";
+            musteri3.Soyad = "mati";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (var mus in musteriler)
+            {
+                Console.WriteLine(mus.Ad+" "+ mus.Soyad);
+            }
+            Console.WriteLine("*************************************************");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriSil(musteri2);
+        }
+    }
+}
